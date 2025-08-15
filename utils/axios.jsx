8fdf737d3e3f29c7561b975f3f8aa2ae7aa3aef6
binod <br163406@gmail.com>
2025-08-14 19:40:30 -1200

    setup for url in axios file with interceptors

---
+++ utils/axios.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: 'https://some-domain.com/api/',
+});
+
+instance.interceptors.request.use(function (config) {
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
+  });
+
+instance.interceptors.response.use(function (response) {
+    return response;
+  }, function (error) {
+    return Promise.reject(error);
+  });
+
+export default instance;
